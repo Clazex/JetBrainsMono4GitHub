9af2ae9567f2c34c1ed338a6af71bbbe153c5584
Clazex <clazex.y@gmail.com>
2022-01-23 20:12:56 +0800

    Version 1.1.0

--- script.user.js
+++ script.user.js
@@ -1,11 +1,17 @@
 // ==UserScript==
 // @name         JetBrainsMono4GitHub
 // @namespace    https://userscript.clazex.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Change GitHub code view font to JetBrains Mono
 // @license      MIT
 // @author       Clazex
+// @copyright    2022, Clazex
+// @updateURL    https://cdn.jsdelivr.net/gh/Clazex/JetBrainsMono4GitHub@latest/script.user.js
+// @downloadURL  https://cdn.jsdelivr.net/ghClazex/JetBrainsMono4GitHub@latest/script.user.js
+// @supportURL   https://github.com/Clazex/JetBrainsMono4GitHub/issues
+// @homepage     https://github.com/Clazex/JetBrainsMono4GitHub
 // @match        https://github.com/*
+// @icon         https://github.githubassets.com/favicons/favicon.png
 // @grant        none
 // ==/UserScript==
 
