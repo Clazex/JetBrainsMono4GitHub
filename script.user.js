cd497437f10611f9163e23e0327d4e6ec3139b93
Clazex <clazex.y@gmail.com>
2022-06-27 20:52:57 +0800

    Version 1.3.0

--- script.user.js
+++ script.user.js
@@ -1,29 +1,22 @@
 // ==UserScript==
-// @name         JetBrainsMono4GitHub
-// @namespace    https://userscript.clazex.net/
-// @version      1.2.0
-// @description  Change GitHub code view font to JetBrains Mono
-// @license      MIT
-// @author       Clazex
-// @copyright    2022, Clazex
-// @updateURL    https://cdn.jsdelivr.net/gh/Clazex/JetBrainsMono4GitHub@latest/script.user.js
-// @downloadURL  https://cdn.jsdelivr.net/ghClazex/JetBrainsMono4GitHub@latest/script.user.js
-// @supportURL   https://github.com/Clazex/JetBrainsMono4GitHub/issues
-// @homepage     https://github.com/Clazex/JetBrainsMono4GitHub
-// @match        https://github.com/*
-// @icon         https://github.githubassets.com/favicons/favicon.png
-// @grant        none
+// @name          JetBrainsMono4GitHub
+// @namespace     https://userscript.clazex.net/
+// @version       1.3.0
+// @description   Change GitHub code view font to JetBrains Mono
+// @license       MIT
+// @author        Clazex
+// @copyright     2022, Clazex
+// @icon          https://github.githubassets.com/favicons/favicon.png
+// @downloadURL   https://cdn.jsdelivr.net/gh/Clazex/JetBrainsMono4GitHub@latest/script.user.js
+// @updateURL     https://cdn.jsdelivr.net/gh/Clazex/JetBrainsMono4GitHub@latest/script.user.js
+// @supportURL    https://github.com/Clazex/JetBrainsMono4GitHub/issues
+// @homepage      https://github.com/Clazex/JetBrainsMono4GitHub
+// @run-at        document-body
+// @grant         GM_addStyle
+// @match         https://github.com/*
 // ==/UserScript==
 
 (function() {
   "use strict";
-
-  var fonts = document.createElement("link");
-  fonts.href = "https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap";
-  fonts.rel = "stylesheet";
-  document.head.appendChild(fonts);
-
-  var style = document.createElement("style");
-  style.innerHTML = ".blob-code-inner { font-family: 'JetBrains Mono', monospace; }";
-  document.body.appendChild(style);
+  GM_addStyle(".blob-code-inner { font-family: 'JetBrains Mono', monospace; }");
 })();
