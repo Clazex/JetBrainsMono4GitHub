8dfa1fc64aa10385ddeabfb545c35964f33db22d
Clazex <clazex.y@gmail.com>
2023-03-02 22:03:47 +0800

    Support markdown code block and customization

--- script.user.js
+++ script.user.js
@@ -1,22 +1,87 @@
 // ==UserScript==
 // @name          JetBrainsMono4GitHub
 // @namespace     https://userscript.clazex.net/
-// @version       1.5.0
+// @version       2.0.0
 // @description   Change GitHub code view font to JetBrains Mono
 // @license       MIT
 // @author        Clazex
 // @copyright     2022, Clazex
 // @icon          https://github.githubassets.com/favicons/favicon.png
-// @downloadURL   https://cdn.jsdelivr.net/gh/Clazex/JetBrainsMono4GitHub@latest/script.user.js
-// @updateURL     https://cdn.jsdelivr.net/gh/Clazex/JetBrainsMono4GitHub@latest/script.user.js
+// @downloadURL   https://raw.githubusercontent.com/Clazex/JetBrainsMono4GitHub/main/script.user.js
+// @updateURL     https://raw.githubusercontent.com/Clazex/JetBrainsMono4GitHub/main/script.user.js
 // @supportURL    https://github.com/Clazex/JetBrainsMono4GitHub/issues
 // @homepage      https://github.com/Clazex/JetBrainsMono4GitHub
 // @run-at        document-body
 // @grant         GM_addStyle
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @grant         GM_registerMenuCommand
+// @grant         GM_unregisterMenuCommand
 // @match         https://github.com/*
 // ==/UserScript==
 
 (function() {
-  "use strict";
-  GM_addStyle(".blob-code-inner,.CodeMirror-lines,.react-code-text { font-family: 'JetBrains Mono', monospace !important; font-size: 1em !important; }");
+  'use strict';
+
+  const DEFAULT_FONT = `'Jetbrains Mono'`;
+
+  function getFont() {
+    return GM_getValue('font', DEFAULT_FONT);
+  }
+
+  function setFont(font) {
+    if (getFont() === font) {
+      return;
+    }
+
+    GM_setValue('font', font);
+    applyFont(font);
+  }
+
+  let style;
+  function applyFont(font = getFont()) {
+    style?.remove();
+    style = GM_addStyle(
+      `.blob-code-inner, .CodeMirror-lines, .highlight pre, .react-code-text { font-family: ${font}, monospace !important }`
+      + '.blob-code-inner, .CodeMirror-lines, .react-code-text { font-size: 1em !important; }'
+    );
+
+    unregisterMenuCommand();
+    registerMenuCommand(font);
+  }
+
+  function updateFont() {
+    const oldFont = getFont();
+    let newFont = '';
+
+    while (true) {
+      newFont = prompt('Specify font family', oldFont);
+
+      if (newFont == null) {
+        return;
+      }
+
+      if (newFont.length > 0) {
+        break;
+      }
+    }
+
+    setFont(newFont);
+  }
+
+  let menuCommandId = null;
+  function registerMenuCommand(font) {
+    menuCommandId = GM_registerMenuCommand("Current font: " + font, updateFont);
+  }
+
+  function unregisterMenuCommand() {
+    if (menuCommandId === null) {
+      return;
+    }
+
+    GM_unregisterMenuCommand(menuCommandId);
+    menuCommandId = null;
+  }
+
+  applyFont();
 })();
